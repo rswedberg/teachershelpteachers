eb59b4b4abe6dbd0d4ffaa3b31e036783082a6ae
Riley <rswedberg@unomaha.edu>
2021-07-23 13:33:12 -0500

    Unit and component testing files started

--- test_component.py
+++ test_component.py
@@ -1,33 +1,61 @@
 import unittest
 from flask import current_app, url_for
-from app import create_app
+from app import app, create_app
 
 class TestComponent(unittest.TestCase):
     def setUp(self):
         self.app = create_app()
-        self.app_context = self.app.app_context()
+        self.app_context = self.app.test_request_context()
         self.app_context.push()
-        self.client = self.app.test_client(use_cookies=False)
+        self.client = self.app.test_client(use_cookies=True)
 
-    # Test to make index page show up
+    # Test to verify redirect to login page
     def test_index(self):
-        self.client.get(url_for('index'))
-        self.assertTrue('Create New Question' in create)
+        # Initiate app to run
+        tester = app.test_client(self)
+        # Run get request for / route
+        response = tester.get('/')
+        # Look for string in html file
+        self.assertIn(b'Login with Google', response.data)
+
+    # Test to verify instructions are given for the create page
+    def test_create_view(self):
+        # Initiate app to run
+        tester = app.test_client(self)
+        # Run get request for /create route
+        response = tester.get('/create')
+        # Look for string in html file
+        self.assertIn(b'Instructions:', response.data)
 
     # Test create route for int
     def test_create_int(self):
-        response = self.client.post(url_for('create'), data={
+        # Initiate app to run
+        tester = app.test_client(self)
+        # Run post request for /create route passing values for all request.form
+        response = tester.post('/create', data={
             'category': 'test question',
-            'raw_question': 'int num = @A;---A, int, 5, 5'
+            'code': 'int num = @A;---A, int, 5, 5',
+            'action': 'view'
         }, follow_redirects=True)
-        self.assertTrue('int num = 6;' in response.data)
-        print(response)
+        # Look for string in html to verify previewed code looks as expected
+        self.assertIn(b'int num = 5;', response.data)
 
+    # Test create route for float
+    def test_create_float(self):
+        # Initiate app to run
+        tester = app.test_client(self)
+        # Run post request for /create route passing values for all request.form
+        response = tester.post('/create', data={
+            'category': 'test question',
+            'code': 'double num = @A;---A, float, 1, 3.3, 3.3',
+            'action': 'view'
+        }, follow_redirects=True)
+        # Look for string in html to verify previewed code looks as expected
+        self.assertIn(b'double num = 3.3;', response.data)
+
+    # Test that app exists 
     def test_app_exists(self):
         self.assertFalse(current_app is None)
-
-    def test_app_is_testing(self):
-        self.assertTrue(current_app.config['TESTING'])
 
     def tearDown(self):
         self.app_context.pop()
--- test_unit.py
+++ test_unit.py
@@ -1,3 +1,46 @@
 from db import get_db, close_db, init_db, init_db_command, init_app
 from db_execute import User, Questions
+from app import app, create_app, generateQuestion
 import unittest
+
+class TestUnit(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app_context = self.app.test_request_context()
+        self.app_context.push()
+        self.client = self.app.test_client(use_cookies=True)
+
+    # Test create method for Questions table
+    def test_questions_create(self):
+        # Set values to be stored in database
+        db_author = "author.email"
+        db_category = "test category"
+        db_question = "test question"
+        # Call method to be tested
+        Questions.create(db_author, db_category, db_question)
+        # Initiate database to be called in current test
+        db = get_db()
+        # Test author value
+        author = db.execute(
+            "SELECT author FROM questions WHERE question = ?", (db_question,)
+        ).fetchone()[0]
+        self.assertEqual(db_author, author)
+        # Test category value
+        category = db.execute(
+            "SELECT category FROM questions WHERE question = ?", (db_question,)
+        ).fetchone()[0]
+        self.assertEqual(db_category, category)
+        # Test question value
+        question = db.execute(
+            "SELECT question FROM questions WHERE author = ?", (db_author,)
+        ).fetchone()[0]
+        self.assertEqual(db_question, question)
+        # Clear test values from database
+        db.execute(
+            "DELETE FROM questions WHERE author = ?", (db_author,)
+        )
+        db.commit()
+
+
+if __name__ == "__main__":
+    unittest.main()
